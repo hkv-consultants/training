83ff8e57ef34fbf590ada7304378fc79fbb78bd1
ansink <j.ansink@hkv.nl>
2013-08-19 14:08:39 +0200

    Modified admin:wq

--- training/tijdreeksplot/admin.py
+++ training/tijdreeksplot/admin.py
@@ -5,3 +5,13 @@ from .models import Location
 class LocationAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Location, LocationAdmin)
+
+
+class RecordValueAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(RecordValue, RecordValueAdmin)
+
+
+class RecordValueListAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(RecordValueList, RecordValueListAdmin)
