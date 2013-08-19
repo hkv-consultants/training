94e4915cf4e0f38d6c219710ce11fd248514dbcb
Job Verkaik <j.verkaik@hkv.nl>
2013-08-19 13:30:49 +0200

    Location class added

--- training/tijdreeksplot/models.py
+++ training/tijdreeksplot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+	"""
+	This class represents a location
+	"""
+	
+	lat = models.DecimalField(max_digits=10, decimal_places=6)
+	lon = models.DecimalField(max_digits=10, decimal_places=6)
+	name = models.CharField(max_length=64)
+	
+	def __unicode__(self):
+		return self.name
+	
