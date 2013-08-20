ac4faa34141a06b5aa59373b735065e16843207d
ansink <j.ansink@hkv.nl>
2013-08-20 10:45:34 +0200

    RecordvalueList view

--- tijdreeksplot/urls.py
+++ tijdreeksplot/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import location_list
+from .views import location_list, RecordValueListList
 
 
 urlpatterns = patterns('',
-    url(r'^$', location_list),
+    url(r'^locations/$', location_list),
+    url(r'^locations/([\w-]+)/$', RecordValueListList.as_view()),
 )
--- tijdreeksplot/views.py
+++ tijdreeksplot/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 from django.template import RequestContext
 
-from .models import Location
+from .models import Location, RecordValueList
 
 
 def location_list(request):
@@ -10,5 +12,13 @@ def location_list(request):
 
     context = {'locations':locations}
     return render_to_response('tijdreeksplot/location_list.html',
-	                          context,
-                          	  context_instance=RequestContext(request))
+                              context,
+                              context_instance=RequestContext(request))
+
+class RecordValueListList(ListView):
+    model = RecordValueList
+
+    def get_queryset(self):
+        qs = super(RecordValueListList, self).get_queryset()
+        self.location = get_object_or_404(Location, name=self.args[0])
+        return qs.filter(location=self.location)
