555acdd7662e9fa482536be31359c6c07ee015f2
ansink <j.ansink@hkv.nl>
2013-08-20 11:29:05 +0200

    added urls

--- tijdreeksplot/urls.py
+++ tijdreeksplot/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import location_list, LocationDetail
+from .views import location_list, LocationDetail, RecordValueListDetail
 
 
 urlpatterns = patterns('',
     url(r'^locations/$', location_list),
     url(r'^locations/(?P<slug>[\w-]+)/$', LocationDetail.as_view(), name='location-detail'),
+    url(r'^locations/(?P<location_slug>[\w-]+)/(?P<slug>[\w-]+)/$', RecordValueListDetail.as_view(), name='recordvaluelist-detail'),
 )
