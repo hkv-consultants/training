e49ebaf654c711332c1fa8730ab7d0ac69b27c7c
ansink <j.ansink@hkv.nl>
2013-08-20 11:07:53 +0200

    get_absolute_url added

--- tijdreeksplot/models.py
+++ tijdreeksplot/models.py
@@ -2,42 +2,48 @@ from django.db import models
 
 
 class Location(models.Model):
-	"""
-	This class represents a location
-	"""
+    """
+    This class represents a location
+    """
 
-	name = models.CharField(max_length=64)
-	slug = models.SlugField(max_length=64, unique=True)
-	lat = models.DecimalField(max_digits=10, decimal_places=6)
-	lon = models.DecimalField(max_digits=10, decimal_places=6)
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
+    lat = models.DecimalField(max_digits=10, decimal_places=6)
+    lon = models.DecimalField(max_digits=10, decimal_places=6)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('location-detail', (), {
+            'slug': self.slug
+        })
 
 
 class RecordValueList(models.Model):
-	"""
-	List RecordValues for a specific location.
-	This is referred to by RecordValue
-	"""
+    """
+    List RecordValues for a specific location.
+    This is referred to by RecordValue
+    """
 
-	name = models.CharField(max_length=64)
-	slug = models.SlugField(max_length=64, unique=True)
-	location = models.ForeignKey(Location)
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
+    location = models.ForeignKey(Location)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 
 class RecordValue(models.Model):
-	"""
-	This class represents a time labelled single record
-	"""
+    """
+    This class represents a time labelled single record
+    """
 
-	value_list = models.ForeignKey(RecordValueList)
-	datetime = models.DateTimeField()
-	value = models.FloatField()
+    value_list = models.ForeignKey(RecordValueList)
+    datetime = models.DateTimeField()
+    value = models.FloatField()
 
-	def __unicode__(self):
-		return "%s, %s: %f" % (self.value_list, self.datetime, self.value)
+    def __unicode__(self):
+        return "%s, %s: %f" % (self.value_list, self.datetime, self.value)
 
