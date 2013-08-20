2778d32f64c655135ecd4eb95ffddef41b0dd644
ansink <j.ansink@hkv.nl>
2013-08-20 11:50:55 +0200

    added back button

--- tijdreeksplot/urls.py
+++ tijdreeksplot/urls.py
@@ -3,7 +3,7 @@ from .views import location_list, LocationDetail, RecordValueListDetail
 
 
 urlpatterns = patterns('',
-    url(r'^locations/$', location_list),
+    url(r'^locations/$', location_list, name='location-list'),
     url(r'^locations/(?P<slug>[\w-]+)/$', LocationDetail.as_view(), name='location-detail'),
     url(r'^locations/(?P<location_slug>[\w-]+)/(?P<slug>[\w-]+)/$', RecordValueListDetail.as_view(), name='recordvaluelist-detail'),
 )
